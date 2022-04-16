dae4f56c5502a7079a93a174682b7b7257b921a1
ww <13540025344@163.com>
2022-04-16 22:28:35 +0800

    完善布谷鸟散列

--- 数据结构与算法/dataStructuresAndAlgorithm/src/main/java/hashTable/CuckooHashTable.java
+++ 数据结构与算法/dataStructuresAndAlgorithm/src/main/java/hashTable/CuckooHashTable.java
@@ -3,6 +3,8 @@ package hashTable;
 import hashTable.hashFamily.IHashFamily;
 import utils.PrimeUtil;
 
+import java.util.Random;
+
 /**
  * 布谷鸟散列
  */
@@ -65,20 +67,132 @@ public class CuckooHashTable <T> {
     }
 
     /**
+     * 插入元素
+     * @param t
+     * @return
+     */
+    public boolean insert (T t) {
+
+        //判断是否包含该元素
+        if (contains(t)) {
+
+            return false;
+        }
+
+        //当装填因子大于MAX_LOAD时先扩容
+        if (currentSize >= hashTable.length * MAX_LOAD) {
+
+            expand();
+        }
+
+        return insertHelper(t);
+    }
+
+    /**
+     * 判断hashtable中是否包含t
+     * @param t
+     * @return
+     */
+    public boolean contains (T t) {
+
+        return findPos(t) != -1;
+    }
+
+    /**
+     * 移除t
+     * @param t
+     * @return
+     */
+    public boolean remove (T t) {
+
+        int pos = findPos(t);
+
+        if (pos != -1) {
+
+            hashTable[pos] = null;
+            currentSize --;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 添加元素
+     * @param t
+     * @return
+     */
+    private boolean insertHelper (T t) {
+
+        int numberOfFunctions = hashFunctions.getNumberOfFunctions();
+        //最大插入循环次数
+        final int COUNT_INSERT = 100;
+        int rehashs = 0;
+        Random r = new Random();
+        T temp;
+
+        while (true){
+            //记录上一次插入的位置
+            int lastPos = -1;
+            int index;
+            for (int i = 0; i < COUNT_INSERT; i++) {
+
+                //查询在表中是否还有对应空位，有的话直接插入
+                for (int j = 0; j < numberOfFunctions; j++) {
+
+                    index = hashFunctions.hash(t, i);
+                    T t1 = hashTable[index];
+
+                    if (t1 == null) {
+
+                        t1 = t;
+                        currentSize ++;
+                        return true;
+                    }
+                }
+
+                //没有空位的情况下，随机选择hash函数对其中值进行替换，当index和之前的index相同时重选
+                int j = 0;
+                do {
+
+                    index = myHash(t, r.nextInt(numberOfFunctions));
+                } while (index != lastPos && j++ < 5);
+
+                temp = hashTable[index];
+                hashTable[index] = t;
+                t = temp;
+            }
+
+            //超过最大循环次数，对表进行扩容处理
+            if (++rehashs > ALLOWED_REHASHES) {
+                expand();
+                rehashs = 0;
+            } else {
+                rehash();
+            }
+        }
+    }
+
+    /**
      * 找到t所在位置
      * 存在返回所在位置
      * 不存在返回-1
      * @param t
      * @return
      */
-    public int findPos (T t) {
+    private int findPos (T t) {
 
-        int index = hashFunctions.hash(t, hashTable.length);
+        int l = hashFunctions.getNumberOfFunctions();
 
-        if (t.equals(hashTable[index])) {
-            return index;
-        }else
-            return -1;
+        for (int i = 0; i < l; i++) {
+
+            if (t.equals(hashTable[myHash(t, i)])) {
+
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     /**
@@ -108,5 +222,53 @@ public class CuckooHashTable <T> {
     private void allocateArray (int size) {
 
         hashTable = (T[]) new Object[size];
+    }
+
+    /**
+     * 返回第which函数计算的t的hash值
+     * @param t
+     * @param which
+     * @return
+     */
+    private int myHash (T t, int which) {
+
+        int hash = hashFunctions.hash(t, which);
+
+        int hashCode = hash % hashTable.length;
+
+        if (hashCode < 0) {
+
+            hashCode += hashTable.length;
+        }
+
+        return hashCode;
+    }
+
+    /**
+     * 对hash表进行完全扩容
+     */
+    private void expand() {
+        rehash ((int) (hashTable.length/MAX_LOAD));
+    }
+
+    /**
+     * 对hash表进行小幅度再散列
+     */
+    private void rehash() {
+        rehash (hashTable.length + 1);
+    }
+
+    /**
+     * 对hash表进行指定扩展后容量的再散列
+     */
+    private void rehash(int size) {
+
+        T[] old = hashTable;
+        hashTable = (T[]) new Object[PrimeUtil.nextPrime(size)];
+
+        for (int i = 0; i < old.length; i++) {
+
+            insert(old[i]);
+        }
     }
 }
